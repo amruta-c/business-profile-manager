187b6fe8395a03b889352f3fb88a4b30d9538b4b
Amruta Chougale <Amruta.Chougale@target.com>
2023-08-15 13:07:10 +0530

    Refactored code

--- src/main/java/com/intuit/businessprofilemanager/repository/AddressRepository.java
+++
@@ -1,7 +0,0 @@
-//package com.intuit.businessprofilemanager.repository;
-//
-//import com.intuit.businessprofilemanager.entity.AddressEntity;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface AddressRepository extends JpaRepository<AddressEntity, Long> {
-//}
